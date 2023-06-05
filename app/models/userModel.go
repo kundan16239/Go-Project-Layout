068d433e99b99f3931ff08535f794a80772b2281
kundan16239 <86150290+kundan16239@users.noreply.github.com>
2023-06-05 06:27:15 +0530

    userModel.go

--- app/models/userModel.go
+++ app/models/userModel.go
@@ -20,19 +20,3 @@ type UserExist struct {
 type RefreshToken struct {
 	RefreshToken string `json:"refreshToken" validate:"required,jwt"`
 }
-
-type CreateProfile struct {
-	Username          string   `json:"username" validate:"required,alphanum,min=3,max=15"`
-	Bio               string   `json:"bio" validate:"required,max=200"`
-	Language          []string `json:"language" validate:"required,min=1,max=3"`
-	Category          []string `json:"category" validate:"required,min=1,max=10"`
-	ApplyVerification string   `json:"applyVerification" validate:"required,boolean"`
-}
-
-type FollowProfile struct {
-	UserId string `json:"userId" validate:"required,len=24"`
-}
-
-type UnfollowProfile struct {
-	UserId string `json:"userId" validate:"required,len=24"`
-}
